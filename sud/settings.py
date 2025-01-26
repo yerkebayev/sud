598150e3a7cec23ab9d1a6b97635a48a767223e7
yerkebayev <maratpyn@gmail.com>
2025-01-26 13:04:32 +0500

    deploy

--- sud/settings.py
+++ sud/settings.py
@@ -27,9 +27,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = 'django-insecure-x&bn_ara2s&7drj)cp&3n$**%mb+93$((!3i4=*3g1x(^n1h=v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['marat8jh.pythonanywhere.com']
+
 
 
 # Application definition
@@ -135,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
