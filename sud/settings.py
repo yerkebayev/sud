3024a5fec327119890f21bf22a1e1033c7e2f596
yerkebayev <maratpyn@gmail.com>
2025-02-02 12:12:23 +0500

    fix

--- sud/settings.py
+++ sud/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,7 +45,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'django_filters',
-    'corsheaders',
     'news_ru',
     'news_uz',
     'news_oz',
