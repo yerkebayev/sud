98b21af63747a50921f5fe83acd6a776f09c4d1c
yerkebayev <maratpyn@gmail.com>
2025-02-02 12:13:12 +0500

    fix

--- sud/settings.py
+++ sud/settings.py
@@ -62,7 +62,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:4174",  # Allow your frontend's local development URL
+    "https://marat8jh.pythonanywhere.com",  # Allow your deployed backend
+]
 CORS_ALLOW_METHODS = [
     "GET",
     "POST",
